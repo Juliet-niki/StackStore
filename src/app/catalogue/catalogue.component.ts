ecd1669b7a8e1efa6e5724583f9b7c362943c05c
Juliet-niki <nwajully@gmail.com>
2025-07-04 02:29:33 +0100

    created a signle card

--- src/app/catalogue/catalogue.component.ts
+++ src/app/catalogue/catalogue.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-catalogue',
   standalone: true,
-  imports: [],
+  imports: [NgFor, MatButtonModule, MatTooltipModule, MatIconModule],
   templateUrl: './catalogue.component.html',
   styleUrl: './catalogue.component.css',
 })
-export class CatalogueComponent {}
+export class CatalogueComponent {
+  rating = 0;
+  stars = new Array(5);
+
+  setRating(value: number) {
+    this.rating = value;
+  }
+}
