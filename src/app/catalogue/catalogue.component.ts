c10fa2ae1a3bd8b2617daed11a32882963f6a157
Juliet-niki <nwajully@gmail.com>
2025-07-05 03:25:57 +0100

    add to cart for catalogue page

--- src/app/catalogue/catalogue.component.ts
+++ src/app/catalogue/catalogue.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product.model';
@@ -13,6 +13,7 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [
     NgFor,
+    NgIf,
     MatButtonModule,
     MatTooltipModule,
     MatIconModule,
@@ -23,6 +24,74 @@ import { RouterLink } from '@angular/router';
   styleUrl: './catalogue.component.css',
 })
 export class CatalogueComponent implements OnInit {
+  loadingMap: { [slug: string]: boolean } = {};
+  buttonVisibleMap: { [slug: string]: boolean } = {};
+  itemSelectorMap: { [slug: string]: boolean } = {};
+  numberOfItemsMap: { [slug: string]: number } = {};
+
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.loadProducts().subscribe((data) => {
+      this.products = data;
+
+      for (const product of this.products) {
+        const slug = product.slug;
+
+        this.loadingMap[slug] = false;
+        this.buttonVisibleMap[slug] = true;
+        this.itemSelectorMap[slug] = false;
+        this.numberOfItemsMap[slug] =
+          this.productService.getProductQuantity(slug);
+      }
+    });
+  }
+
+  openItemSelector(slug: string): void {
+    this.loadingMap[slug] = true;
+
+    setTimeout(() => {
+      this.buttonVisibleMap[slug] = false;
+      this.itemSelectorMap[slug] = true;
+
+      if (this.numberOfItemsMap[slug] === 0) {
+        this.numberOfItemsMap[slug] = 1;
+        this.productService.setProductQuantity(slug, 1);
+
+        const currentCartCount = this.productService.getCartCount();
+        this.productService.setCartCount(currentCartCount + 1);
+      }
+
+      this.loadingMap[slug] = false;
+    }, 1500);
+  }
+
+  add(slug: string): void {
+    this.numberOfItemsMap[slug]++;
+    this.productService.setProductQuantity(slug, this.numberOfItemsMap[slug]);
+
+    const newTotal = this.productService.getCartCount() + 1;
+    this.productService.setCartCount(newTotal);
+  }
+
+  remove(slug: string): void {
+    if (this.numberOfItemsMap[slug] > 0) {
+      this.numberOfItemsMap[slug]--;
+      this.productService.setProductQuantity(slug, this.numberOfItemsMap[slug]);
+
+      const newTotal = this.productService.getCartCount() - 1;
+      this.productService.setCartCount(newTotal);
+    }
+
+    if (this.numberOfItemsMap[slug] === 0) {
+      this.itemSelectorMap[slug] = false;
+      this.buttonVisibleMap[slug] = true;
+      this.loadingMap[slug] = false;
+    }
+  }
+
   updateRating(product: Product, newRating: number) {
     product.rating = newRating;
 
@@ -38,15 +107,5 @@ export class CatalogueComponent implements OnInit {
       localStorage.setItem('products', JSON.stringify(allProducts));
     }
     console.log(allProducts);
-  }
-
-  products: Product[] = [];
-
-  constructor(private productService: ProductService) {}
-
-  ngOnInit(): void {
-    this.productService.loadProducts().subscribe((data) => {
-      this.products = data;
-    });
   }
 }
